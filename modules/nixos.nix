31e87901091da95827020d04ff6b795f06ed144f
charlotte whittleman <charlotte@whittleman.net>
2026-03-25 09:17:19 -0500

    fix: enable SDDM when sddmTheme is set
    Setting sddmTheme implies the user wants SDDM as the display manager,
    but the module only configured the theme without enabling the service.

--- modules/nixos.nix
+++ modules/nixos.nix
@@ -43,6 +43,7 @@ in
     }
 
     (lib.mkIf (cfg.sddmTheme != null) {
+      services.displayManager.sddm.enable = true;
       services.displayManager.sddm.theme =
         lib.last (lib.splitString "/" cfg.sddmTheme);
 
